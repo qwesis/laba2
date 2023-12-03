10d5d21b65d63ad1fa63825ff5610c5a5e62ec5a
qwesis <ki4gin@yandex.ru>
2023-12-03 18:57:28 +0300

    fix_ForCube2.cs

--- test/Assets/ForCube2.cs
+++ test/Assets/ForCube2.cs
@@ -13,7 +13,8 @@ public class ForCube2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam.SetActive(false);
+        bcam.SetActive(true);
     }
 
     // Update is called once per frame
